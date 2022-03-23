791028e2b1b66073052823f6550b0fd3370d45bd
Mustafa GÖK <mustafa.gok.ee@gmail.com>
2022-03-23 23:15:56 +0300

    Update List(Flatten).py

--- List(Flatten).py
+++ List(Flatten).py
@@ -8,34 +8,3 @@ def flat(l):
 
 flat([[1,'a',['cat'],2],[[[3]],'dog'],4,5])
 print(list_flat)
-
-#############################################
-
-liste = [[1,'a',['cat'],2],[[[3]],'dog'],4,5]
-list_flat = []
-for i in range(len(liste)):
-    try:
-        for j in range(len(liste[i])):
-            list_flat.append(liste[i][j])
-    except TypeError:
-        list_flat.append(liste[i])
-
-list_flat2 = []
-for i in range(len(list_flat)):
-    try:
-        if type(list_flat[i])==list:
-            for j in range(len(list_flat[i])):
-                list_flat2.append(list_flat[i][j])
-        else:
-            list_flat2.append(list_flat[i])
-    except TypeError:
-        list_flat2.append(list_flat[i])
-
-list_flat3 = []
-for i in range(len(list_flat2)):
-    if type(list_flat2[i]) == list :
-        for j in range(len(list_flat2[i])):
-            list_flat3.append(list_flat2[i][j])
-    else:
-        list_flat3.append(list_flat2[i])
-print(list_flat3)
